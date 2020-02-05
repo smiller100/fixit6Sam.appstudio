66a5c6ffbd8fd2be5cfae21d1edb98a4c230ec02
smiller100 <srm08530@creighton.edu>
2020-02-05 16:53:34 -0600

    Final first scenario update

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -7,26 +7,33 @@ console.log(withMarmaduke);
 //> remove "Vinny" from the array. 
 let removeVinny = petNames.splice(5,1);
 console.log(petNames);
+console.log(`The action taken was: Remove Vinny\nThe remaining data are:\n${petNames}`);
 
 //> remove "Marty" from the array.
 let removeMarty = petNames.splice(3,1);
 console.log(petNames);
+console.log(`The action taken was: Remove Marty\nThe remaining data are:\n${petNames}`);
 
 //> add "Nancy" to the front of the array.
 let addNancy = petNames.unshift("Nancy");
 console.log(petNames);
+console.log(`The action taken was: Add Nancy to the front\nThe existing data is:\n${petNames}`);
 
 //> add their name to the end of the array.
 let addSam = petNames.push("Sam");
 console.log(petNames);
+console.log(`The action taken was: Add Sam to the end\nThe existing data is:\n${petNames}`);
 
 //> tell the user the array index where "Riley" is located in the array.
 alert(petNames.indexOf("Riley"));
 console.log(petNames.indexOf("Riley"));
+console.log(`The action taken was: Tell us where Riley is located\nThe output is:\n${petNames.indexOf("Riley")}`);
 
 //> tell the user the array index where "Cindy" is located. This should return-1 since "Cindy" is not in the array
 alert(petNames.indexOf("Cindy"));
 console.log(petNames.indexOf("Cindy"));
+console.log(`The action taken was: Tell us where Cindy is located\nThe output is:\n${petNames.indexOf("Cindy")}`);
 
 //> show the 'withMarmaduke' array data.
 console.log(withMarmaduke);
+console.log(`The action taken was: Show us the withMarmaduke array\nThe output is:\n${withMarmaduke}`);
