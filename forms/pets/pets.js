ccaaf92a8ef890d9a13dcb22d8bb586f08b356cc
smiller100 <srm08530@creighton.edu>
2020-02-05 16:40:05 -0600

    Second update

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -1,17 +1,32 @@
+//
 let petNames = ['Darcy','Jack','Jerry','Marty','Vito','Vinny','Riley','Saddie','Marge','Sam'];
 let withMarmaduke = ['Darcy','Jack','Jerry','Marty','Vito','Vinny','Riley','Saddie','Marge','Sam','Marmaduke'];
 console.log(petNames[5]);
 console.log(withMarmaduke);
-/*
-> remove "Vinny" from the array.
-> remove "Marty" from the array.
-> add "Nancy" to the front of the array.
-> add their name to the end of the array.
-> tell the user the array index where "Riley" is located in the array.
-> tell the user the array index where "Cindy" is located. This should return-1 since "Cindy" is not in the array.
-> show the 'withMarmaduke' array data.
-*/
+
+//> remove "Vinny" from the array. 
 let removeVinny = petNames.splice(5,1);
+console.log(petNames);
+
+//> remove "Marty" from the array.
 let removeMarty = petNames.splice(3,1);
+console.log(petNames);
+
+//> add "Nancy" to the front of the array.
 let addNancy = petNames.unshift("Nancy");
+console.log(petNames);
+
+//> add their name to the end of the array.
 let addSam = petNames.push("Sam");
+console.log(petNames);
+
+//> tell the user the array index where "Riley" is located in the array.
+alert(petNames.indexOf("Riley"));
+console.log(petNames.indexOf("Riley"));
+
+//> tell the user the array index where "Cindy" is located. This should return-1 since "Cindy" is not in the array
+alert(petNames.indexOf("Cindy"));
+console.log(petNames.indexOf("Cindy"));
+
+//> show the 'withMarmaduke' array data.
+console.log(withMarmaduke);
